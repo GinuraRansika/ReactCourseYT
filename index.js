0294027f70a61f37ee393861e6847d4b4663a1b3
Ginura Ransika <ginuraransika@gmail.com>
2022-09-01 23:59:57 +0530

    added CDN Links and render a simple sentences into the we page

---
+++ index.js
@@ -0,0 +1,8 @@
+//* allows to render something that looks like html
+/*
+    ! ParameterList
+            ? 1. What do i want to render to the screen
+            ? 2. Where do i want to render it       
+*/
+ReactDOM.render(<h1>Hello, Everyone!</h1>, document.getElementById("root"));
+ReactDOM.render(<p>Hi, my name is Ginura!</p>, document.getElementById("root"));
