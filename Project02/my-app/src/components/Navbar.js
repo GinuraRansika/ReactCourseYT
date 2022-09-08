a89a85cb2b6ca6b9c0fbb3dc750868fd8fea1086
Ginura Ransika <ginuraransika@gmail.com>
2022-09-09 00:47:28 +0530

    Finished the Project02's Navbar

--- Project02/my-app/src/components/Navbar.js
+++ Project02/my-app/src/components/Navbar.js
@@ -1,7 +1,16 @@
 import React from "react"
+import reactLogo from "../images/React-icon.svg.png"
 
 export default function Navbar() {
     return(
-        <h1>Navbar goes Here</h1>
+        <nav className="navbar">
+            <div className="logo">
+                <img src={reactLogo} alt="React Logo"/>
+                <h3 className="logo-name">ReactFacts</h3>
+            </div>
+            <div className="logo-description">
+                <h4 className="description">React Course - Project 2</h4>
+            </div>
+        </nav>
     )
 }
