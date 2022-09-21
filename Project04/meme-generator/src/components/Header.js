02d3a72c44cc3c45a6c5fd1b43fefa42b7e7cc29
Ginura Ransika <ginuraransika@gmail.com>
2022-09-21 19:21:11 +0530

    Started the Meme Generator Project (Project 04) and added the Header Section

---
+++ Project04/meme-generator/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header(){
+    return(
+        <header className="header">
+            <img src={require("../images/troll-face.png")} alt="" className="header-image"/>
+            <h2 className="header-title">Meme Generator</h2>
+            <h4 className="header-project"> React Course - Project 4</h4>
+        </header>
+    )
+}
