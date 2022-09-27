954a8193b5819e494fc3769306648f2a134e0664
Ginura Ransika <ginuraransika@gmail.com>
2022-09-27 22:44:06 +0530

    Lesson 29(ComplexState-Object)- how to apply state that's in the from of an object

---
+++ Lesson29(ComplexState-Object)/index.js
@@ -0,0 +1,36 @@
+function App() {
+    const [contact, setContact] = React.useState({
+        firstName: "John",
+        lastName: "Doe",
+        phone: "+1 (719) 555-1212",
+        email: "itsmyrealname@example.com",
+        isFavorite: false
+    })
+
+    let starIcon = contact.isFavorite?"./images/filled-star.png":"./images/empty-star.png"
+    function toggleFavorite() {
+        console.log("Toggle Favorite")
+    }
+    
+    return (
+        <main>
+            <article className="card">
+                <img src="./images/user.jpg" className="card--image" />
+                <div className="card--info">
+                    <img 
+                        src={starIcon} 
+                        className="card--favorite"
+                        onClick={toggleFavorite}
+                    />
+                    <h2 className="card--name">
+                        {contact.firstName} {contact.lastName}
+                    </h2>
+                    <p className="card--contact">{contact.phone}</p>
+                    <p className="card--contact">{contact.email}</p>
+                </div>
+            </article>
+        </main>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
