0ed33964e6982e35238f44666027ad9c95b36887
Ginura Ransika <ginuraransika@gmail.com>
2022-09-29 20:24:55 +0530

    Added styles using React to the BoxesChallenge

--- Challenges/BoxesChallenge/box-challenge/src/App.js
+++ Challenges/BoxesChallenge/box-challenge/src/App.js
@@ -2,9 +2,25 @@ import './App.css';
 import boxes from'./boxes';
 import React from 'react';
 
-function App() {
+function App(props) {
+
   const [squares, setSquares] = React.useState(boxes)
-  const squareElements = squares.map(square => <div key={square.id} className="square"></div>)
+  //* when we use style in React we don't use style="String"  we use style={{object}}
+  /* 
+    ? you need to use two curly braces first one to enter JavaScript and Second one to put the Object or 
+      * const squareElements = squares.map(square => <div style={{}} key={square.id} className="square"></div>)
+    ? you can create a separate variable and set that equal to an object like below
+      * const styles = {}
+      * const squareElements = squares.map(square => <div style={styles} key={square.id} className="square"></div>)
+  */
+
+    // ! styles need to be in camelCase like we did using dom elements in JS
+        // * example : document.getElementById("id").style.backgroundColor = color;
+    const styles = {
+      backgroundColor: props.darkMode?"#E80F88":"#AF0171",
+    }
+    const squareElements = squares.map(square => <div style={styles} key={square.id} className="square"></div>)
+
   return (
   <main>
     {squareElements}
