5d3a3920cad3d7e88402845a047acc95b7142a65
Ginura Ransika <ginuraransika@gmail.com>
2022-10-02 17:52:35 +0530

    Lesson38(Textarea in React Froms)

---
+++ Lesson38(Forms-Textarea)/index.js
@@ -0,0 +1,72 @@
+function Form() {
+    const [formData, setFormData] = React.useState({
+        firstName: "",
+        lastName: "",
+        email:"",
+        comments:""
+    })
+    console.log(formData)
+
+
+    function handleChange(event){
+        setFormData(prevFormData => {
+            return{
+                ...prevFormData,
+                [event.target.name]: event.target.value,
+            }
+        })
+    }
+
+    return(
+        <form>
+
+            <input 
+                type="text"
+                placeholder="First Name"
+                onChange={handleChange}
+                name="firstName"
+                value={formData.firstName}
+            />
+
+            <input 
+                type="text"
+                placeholder="Last Name"
+                onChange={handleChange}
+                name="lastName"
+                value={formData.lastName}
+            />    
+
+            <input 
+                type="email"
+                placeholder="Email"
+                onChange={handleChange}
+                name="email"
+                value={formData.email}
+            />
+
+            {/* //* In react they slightly changed the textarea input (example: now it's a self closing tag) */}
+            <textarea 
+                placeholder="Any Comments" 
+                onChange={handleChange}
+                name="comments"
+                value={formData.comments} 
+            />
+            
+        </form>
+    )
+}
+
+function App() {
+    return (
+        <main>
+            <Form/>
+        </main>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
